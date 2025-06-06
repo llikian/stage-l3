b8bb5b7e83b5d5137f051e953c54e9e0b61ea200
llikian <pokillian64@gmail.com>
2025-06-06 16:44:43 +0200

    Shader class

---
+++ include/Shader.hpp
@@ -0,0 +1,163 @@
+/***************************************************************************************************
+ * @file  Shader.hpp
+ * @brief Declaration of the Shader class
+ **************************************************************************************************/
+
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+/**
+ * @class Shader
+ * @brief Compiles and link shaders to a shader program that can then be bound. Contains functionality
+ * to change the value of uniforms.
+ */
+class Shader {
+public:
+    /**
+     * @brief Creates a shader program and compiles then attaches the shaders at the specified paths
+     * to it.
+     * @param paths The paths to each of the different shaders.
+     * @param shaders_count The amount of shaders to attach.
+     * @param shader_program_name The name of the shader program.
+     */
+    Shader(const std::string paths[], unsigned int shaders_count, const std::string& shader_program_name);
+
+    /**
+     * @brief Deletes the shader program.
+     */
+    ~Shader();
+
+    /**
+     * @brief Compiles a shader and returns its corresponding id.
+     * @param path The path to the shader file.
+     * @return The shader's corresponding id.
+     */
+    static unsigned int compileShader(const std::string& path);
+
+    /**
+     * @brief Uses the shader program.
+     */
+    void use() const;
+
+    /**
+     * @brief Getter for the id of the shader.
+     * @return The id of the shader.
+     */
+    uint getID() const;
+
+    /**
+     * @brief Getter for the name of the shader.
+     * @return The name of the shader.
+     */
+    std::string getName() const;
+
+    /**
+     * @brief Sets the value of a uniform of any of the available types.
+     * @param uniform The uniform's name.
+     * @param value The new value of the uniform.
+     */
+    template <typename... Value>
+    void setUniform(const std::string& uniform, Value... value);
+
+private:
+    /**
+     * @brief Finds and adds all the shader's uniforms' id's to the map.
+     */
+    void get_uniforms();
+
+    /**
+     * @brief Sets the value of a uniform of type int.
+     * @param location The uniform's location.
+     * @param value The new value of the uniform.
+     */
+    static void set_uniform(int location, int value);
+
+    /**
+     * @brief Sets the value of a uniform of type ivec2.
+     * @param location The uniform's location.
+     * @param x The new x value of the uniform.
+     * @param y The new y value of the uniform.
+     */
+    static void set_uniform(int location, int x, int y);
+
+    /**
+     * @brief Sets the value of a uniform of type ivec3.
+     * @param location The uniform's location.
+     * @param x The new x value of the uniform.
+     * @param y The new y value of the uniform.
+     * @param z The new z value of the uniform.
+     */
+    static void set_uniform(int location, int x, int y, int z);
+
+    /**
+     * @brief Sets the value of a uniform of type ivec4.
+     * @param location The uniform's location.
+     * @param x The new x value of the uniform.
+     * @param y The new y value of the uniform.
+     * @param z The new z value of the uniform.
+     * @param w The new w value of the uniform.
+     */
+    static void set_uniform(int location, int x, int y, int z, int w);
+
+    /**
+     * @brief Sets the value of a uniform of type unsigned int.
+     * @param location The uniform's location.
+     * @param value The new value of the uniform.
+     */
+    static void set_uniform(int location, unsigned int value);
+
+    /**
+     * @brief Sets the value of a uniform of type bool.
+     * @param location The uniform's location.
+     * @param value The new value of the uniform.
+     */
+    static void set_uniform(int location, bool value);
+
+    /**
+     * @brief Sets the value of a uniform of type float.
+     * @param location The uniform's location.
+     * @param value The new value of the uniform.
+     */
+    static void set_uniform(int location, float value);
+
+    /**
+     * @brief Sets the value of a uniform of type vec2.
+     * @param location The uniform's location.
+     * @param x The new x value of the uniform.
+     * @param y The new y value of the uniform.
+     */
+    static void set_uniform(int location, float x, float y);
+
+    /**
+     * @brief Sets the value of a uniform of type vec3.
+     * @param location The uniform's location.
+     * @param x The new x value of the uniform.
+     * @param y The new y value of the uniform.
+     * @param z The new z value of the uniform.
+     */
+    static void set_uniform(int location, float x, float y, float z);
+
+    /**
+     * @brief Sets the value of a uniform of type vec4.
+     * @param location The uniform's location.
+     * @param x The new x value of the uniform.
+     * @param y The new y value of the uniform.
+     * @param z The new z value of the uniform.
+     * @param w The new w value of the uniform.
+     */
+    static void set_uniform(int location, float x, float y, float z, float w);
+
+    unsigned int id;  ///< The shader program's id.
+    std::string name; ///< The shader's name.
+
+    std::unordered_map<std::string, int> uniform_locations; ///< Stores location of uniforms.
+};
+
+template <typename... Value>
+void Shader::setUniform(const std::string& uniform, Value... value) {
+    if(uniform.contains(uniform)) {
+        setUniform(uniform_locations.at(uniform), value...);
+    }
+}
---
+++ src/Shader.cpp
@@ -0,0 +1,186 @@
+/***************************************************************************************************
+ * @file  Shader.cpp
+ * @brief Implementation of the Shader class
+ **************************************************************************************************/
+
+#include "Shader.hpp"
+
+#include <fstream>
+#include <sstream>
+#include <glad/glad.h>
+
+Shader::Shader(const std::string paths[], unsigned int shaders_count, const std::string& shader_program_name = "")
+    : id(glCreateProgram()),
+      name(shader_program_name.empty() ? "shader" + std::to_string(id) : shader_program_name) {
+    /* ---- Shaders ---- */
+    for(unsigned int i = 0 ; i < shaders_count ; ++i) {
+        unsigned int shader_id = compileShader(paths[i]);
+        glAttachShader(id, shader_id);
+        glDeleteShader(shader_id);
+    }
+
+    /* ---- Shader Program ---- */
+    glLinkProgram(id);
+
+    int message_length;
+    glGetProgramiv(id, GL_INFO_LOG_LENGTH, &message_length);
+    if(message_length > 0) {
+        char* message = new char[message_length];
+        glGetProgramInfoLog(id, message_length, nullptr, message);
+        std::string error_message = "Failed to link shader program '" + shader_program_name + ":\n" + message;
+
+        delete[] message;
+        throw std::runtime_error(error_message);
+    }
+
+    get_uniforms();
+}
+
+Shader::~Shader() {
+    glDeleteProgram(id);
+}
+
+unsigned int Shader::compileShader(const std::string& path) {
+    std::string extension = path.substr(path.find_last_of('.') + 1);
+
+    std::string shader_type_name;
+    unsigned int shader_type;
+    switch(extension[0]) {
+        case 'v':
+            shader_type_name = "vertex";
+            shader_type = GL_VERTEX_SHADER;
+            break;
+        case 'f':
+            shader_type_name = "fragment";
+            shader_type = GL_FRAGMENT_SHADER;
+            break;
+        case 't':
+            if(extension[3] == 'c') {
+                shader_type_name = "tesselation control";
+                shader_type = GL_TESS_CONTROL_SHADER;
+            } else {
+                shader_type_name = "tesselation evaluation";
+                shader_type = GL_TESS_EVALUATION_SHADER;
+            }
+            break;
+        case 'c':
+            shader_type_name = "compute";
+            shader_type = GL_COMPUTE_SHADER;
+            break;
+        case 'g':
+            shader_type_name = "geometry";
+            shader_type = GL_GEOMETRY_SHADER;
+            break;
+        default:
+            throw std::runtime_error("Unknown shader extension: " + extension);
+    }
+
+    std::ifstream file(path);
+    if(!file.is_open()) {
+        throw std::runtime_error(std::string("Failed to open " + shader_type_name + " shader."));
+    }
+
+    std::string raw_code = (std::stringstream() << file.rdbuf()).str();
+    const char* code = raw_code.c_str();
+    unsigned int shader_id = glCreateShader(shader_type);
+    glShaderSource(shader_id, 1, &code, nullptr);
+    glCompileShader(shader_id);
+
+    int message_length;
+    glGetShaderiv(shader_id, GL_INFO_LOG_LENGTH, &message_length);
+    if(message_length > 0) {
+        char* message = new char[message_length];
+        glGetShaderInfoLog(shader_id, message_length, nullptr, message);
+        std::string error_message = "Failed to compile " + shader_type_name + " shader '" + path + "':\n" + message;
+
+        delete[] message;
+        throw std::runtime_error(error_message);
+    }
+
+    return shader_id;
+}
+
+void Shader::use() const {
+    glUseProgram(id);
+}
+
+void Shader::get_uniforms() {
+    use();
+
+    int MAX_CHAR;
+    glGetProgramiv(id, GL_ACTIVE_UNIFORM_MAX_LENGTH, &MAX_CHAR);
+
+    int length;
+    int size;
+    unsigned int type;
+    char* uniform_name = new char[MAX_CHAR];
+
+    int count;
+    glGetProgramiv(id, GL_ACTIVE_UNIFORMS, &count);
+
+    for(int i = 0 ; i < count ; ++i) {
+        glGetActiveUniform(id, i, MAX_CHAR, &length, &size, &type, uniform_name);
+
+        if(size == 1) { // Single value
+            uniform_locations.emplace(uniform_name, glGetUniformLocation(id, uniform_name));
+        } else { // Array
+            std::string index;
+            for(int j = 0 ; j < size ; ++j) {
+                uniform_name[length - 2] = '\0';
+                index = uniform_name;
+                index += std::to_string(j) + ']';
+                uniform_locations.emplace(index, glGetUniformLocation(id, index.c_str()));
+            }
+        }
+    }
+
+    delete[] uniform_name;
+}
+
+uint Shader::getID() const {
+    return id;
+}
+
+std::string Shader::getName() const {
+    return name;
+}
+
+void Shader::set_uniform(int location, int value) {
+    glUniform1i(location, value);
+}
+
+void Shader::set_uniform(int location, int x, int y) {
+    glUniform2i(location, x, y);
+}
+
+void Shader::set_uniform(int location, int x, int y, int z) {
+    glUniform3i(location, x, y, z);
+}
+
+void Shader::set_uniform(int location, int x, int y, int z, int w) {
+    glUniform4i(location, x, y, z, w);
+}
+
+void Shader::set_uniform(int location, unsigned int value) {
+    glUniform1ui(location, value);
+}
+
+void Shader::set_uniform(int location, bool value) {
+    glUniform1i(location, static_cast<int>(value));
+}
+
+void Shader::set_uniform(int location, float value) {
+    glUniform1f(location, value);
+}
+
+void Shader::set_uniform(int location, float x, float y) {
+    glUniform2f(location, x, y);
+}
+
+void Shader::set_uniform(int location, float x, float y, float z) {
+    glUniform3f(location, x, y, z);
+}
+
+void Shader::set_uniform(int location, float x, float y, float z, float w) {
+    glUniform4f(location, x, y, z, w);
+}
