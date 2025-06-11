9633836dda06aa746cd3d68b72c0521dcf83627c
llikian <pokillian64@gmail.com>
2025-06-11 14:38:58 +0200

    Added stb image

---
+++ lib/stb/stb_image.cpp
@@ -0,0 +1,7 @@
+/***************************************************************************************************
+ * @file  stb_image.cpp
+ * @brief Implementation file for the stb_image library
+ **************************************************************************************************/
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
