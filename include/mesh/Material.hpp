fefe95821b4273fa6ed040c937481f87090b86f9
llikian <pokillian64@gmail.com>
2025-06-13 13:30:26 +0200

    Material class documentation

--- include/mesh/Material.hpp
+++ include/mesh/Material.hpp
@@ -12,18 +12,25 @@
 
 /**
  * @class Material
- * @brief
+ * @brief Holds information about a material. Allows to specify how an object is rendered.
  */
 struct Material {
+    /**
+     * @brief Sets default values for the material's data.
+     */
     Material();
 
+    /**
+     * @brief Updates a shader's values with the material's data.
+     * @param shader The shader whose uniforms need to be updated.
+     */
     void update_shader_uniforms(const Shader& shader) const;
 
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
-    float specular_exponent;
+    vec3 ambient; ///< Ambient color.
+    vec3 diffuse; ///< Diffuse color.
+    vec3 specular; ///< Specular color
+    float specular_exponent; ///< Specular Exponent.
 
-    Texture ambient_map;
-    Texture diffuse_map;
+    Texture ambient_map; ///< Ambient map.
+    Texture diffuse_map; ///< Diffuse map.
 };
