3121a25600e794440922466ffaad6e90f9a8deaa
llikian <pokillian64@gmail.com>
2025-06-13 13:31:56 +0200

    Documentation for utility

--- include/utility/hash.hpp
+++ include/utility/hash.hpp
@@ -8,6 +8,14 @@
 #include <iostream>
 #include "maths/vec3.hpp"
 
+/**
+ * @brief Class used to hash an ivec3.
+ */
 struct ivec3_hash {
+    /**
+     * @brief Hashing function for an ivec3.
+     * @param vec The ivec3 to hash.
+     * @return The ivec3's hash.
+     */
     std::size_t operator()(const ivec3& vec) const;
 };
