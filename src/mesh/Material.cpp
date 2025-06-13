31e10b2eedfdd3f76ea83fd3ea9f2ed80921176d
llikian <pokillian64@gmail.com>
2025-06-13 10:18:52 +0200

    Removed code for handling default ambient and diffuse maps as it caused a segfault. Will fix later

--- src/mesh/Material.cpp
+++ src/mesh/Material.cpp
@@ -6,7 +6,10 @@
 #include "mesh/Material.hpp"
 
 Material::Material()
-    : ambient(1.0f), diffuse(1.0f), specular(1.0f), specular_exponent(10.0f) { }
+    : ambient(1.0f),
+      diffuse(1.0f),
+      specular(1.0f),
+      specular_exponent(10.0f) { }
 
 void Material::update_shader_uniforms(const Shader& shader) const {
     shader.set_uniform("u_ambient", ambient);
@@ -14,17 +17,14 @@ void Material::update_shader_uniforms(const Shader& shader) const {
     shader.set_uniform("u_specular", specular);
     shader.set_uniform("u_specular_exponent", specular_exponent);
 
-    static Texture default_ambient_map(255, 255, 255);
-    static Texture default_diffuse_map(255, 255, 255);
-
     if(ambient_map.is_default_texture()) {
-        default_ambient_map.bind(0);
+        // TODO : Set a specific texture when no texture is set
     } else {
         ambient_map.bind(0);
     }
 
     if(diffuse_map.is_default_texture()) {
-        default_diffuse_map.bind(1);
+        // TODO : Set a specific texture when no texture is set
     } else {
         diffuse_map.bind(1);
     }
