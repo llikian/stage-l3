d5f675a0316c33585960284046fba1c7d0a7ec65
llikian <pokillian64@gmail.com>
2025-06-18 16:15:20 +0200

    Transformed hash_ivec3 into the general hash_vector3 template class

--- include/utility/hash.hpp
+++ include/utility/hash.hpp
@@ -9,13 +9,18 @@
 #include "maths/vec3.hpp"
 
 /**
- * @brief Class used to hash an ivec3.
+ * @struct vector3_hash
+ * @brief Class used to hash a vector3.
+ * @tparam Type The type of the vector's components.
  */
-struct ivec3_hash {
+template <typename Type>
+struct vector3_hash {
     /**
-     * @brief Hashing function for an ivec3.
-     * @param vec The ivec3 to hash.
-     * @return The ivec3's hash.
+     * @brief Hashing function for an vector3.
+     * @param vec The vector3 to hash.
+     * @return The vector3's hash.
      */
-    std::size_t operator()(const ivec3& vec) const;
+    std::size_t operator()(const vector3<Type>& vec) const {
+        return std::hash<Type>{}(vec.x) ^ (std::hash<Type>{}(vec.y) << 1) ^ (std::hash<Type>{}(vec.z) << 2);
+    }
 };
--- src/utility/hash.cpp
+++ src/utility/hash.cpp
@@ -4,7 +4,3 @@
  **************************************************************************************************/
 
 #include "utility/hash.hpp"
-
-size_t ivec3_hash::operator()(const ivec3& vec) const {
-    return std::hash<int>{}(vec.x) ^ (std::hash<int>{}(vec.y) << 1) ^ (std::hash<int>{}(vec.z) << 2);
-}
