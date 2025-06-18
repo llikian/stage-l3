a0da98c83992447665e5bb366c4621fbed3d91d5
llikian <pokillian64@gmail.com>
2025-06-18 11:03:51 +0200

    CMakeLists declares a macro called 'DEBUG' when compiling in debug mode. Added a header to define debugging macros

---
+++ include/debug.hpp
@@ -0,0 +1,13 @@
+/***************************************************************************************************
+ * @file  debug.hpp
+ * @brief Declaration of debugging macros used for configuring logging
+ **************************************************************************************************/
+
+#pragma once
+
+#ifdef DEBUG
+
+#define DEBUG_LOG_SHADER_LIFETIME
+#define DEBUG_LOG_TEXTURE_LIFETIME
+
+#endif
