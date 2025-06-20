0a1b433c7c63d011f5cdfbc1d03a02a9306b503e
llikian <pokillian64@gmail.com>
2025-06-20 11:02:39 +0200

    Added file for some maths functions. pow2 function

---
+++ include/maths/functions.hpp
@@ -0,0 +1,11 @@
+/***************************************************************************************************
+ * @file  functions.hpp
+ * @brief Declaration of maths functions
+ **************************************************************************************************/
+
+#pragma once
+
+template <typename Type>
+Type pow2(Type value) {
+    return value * value;
+}
