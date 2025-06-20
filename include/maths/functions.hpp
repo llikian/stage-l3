0fcc8da316cde38712c8fba5cb71efaf23086eef
llikian <pokillian64@gmail.com>
2025-06-20 14:47:27 +0200

    Function to create an rgb color with an hsl hue

--- include/maths/functions.hpp
+++ include/maths/functions.hpp
@@ -5,7 +5,17 @@
 
 #pragma once
 
+#include "vec3.hpp"
+
+/**
+ * @brief Computes the square of a value.
+ * @tparam Type The type of the value.
+ * @param value The value to square.
+ * @return value * value.
+ */
 template <typename Type>
 Type pow2(Type value) {
     return value * value;
 }
+
+vec3 hue_to_rgb(unsigned short hue);
--- src/maths/functions.cpp
+++ src/maths/functions.cpp
@@ -4,3 +4,18 @@
  **************************************************************************************************/
 
 #include "maths/functions.hpp"
+#include <cmath>
+
+vec3 hue_to_rgb(unsigned short hue) {
+    float x = 1.0f - std::abs(std::fmod(static_cast<float>(hue) / 60.0f, 2.0f) - 1.0f);
+
+    switch((hue / 60) % 6) {
+        case 0: return vec3(1.0f, x, 0.0f);
+        case 1: return vec3(x, 1.0f, 0.0f);
+        case 2: return vec3(0.0f, 1.0f, x);
+        case 3: return vec3(0.0f, x, 1.0f);
+        case 4: return vec3(x, 0.0f, 1.0f);
+        case 5: return vec3(1.0f, 0.0f, x);
+        default: return vec3();
+    }
+}
