a42f9fc5e931ed1cb1444be808adf9176f654e3a
llikian <pokillian64@gmail.com>
2025-06-25 14:49:14 +0200

    Base code for rendering a terrain

---
+++ include/mesh/Terrain.hpp
@@ -0,0 +1,34 @@
+/***************************************************************************************************
+ * @file  Terrain.hpp
+ * @brief Declaration of the Terrain class
+ **************************************************************************************************/
+
+#pragma once
+
+#include "TriangleMesh.hpp"
+
+/**
+ * @class Terrain
+ * @brief
+ */
+class Terrain {
+public:
+    Terrain(const Shader& shader, float chunk_size, unsigned int chunks_on_line);
+    ~Terrain();
+
+    void draw(const mat4& view_projection) const;
+
+private:
+    unsigned int get_index(unsigned int x, unsigned int y) const;
+
+    const Shader& shader;
+
+    std::vector<vec2> positions;
+    std::vector<unsigned int> indices;
+    unsigned int VAO;
+    unsigned int VBO;
+    unsigned int EBO;
+
+    float chunk_size;
+    unsigned int chunks_on_line;
+};
---
+++ src/mesh/Terrain.cpp
@@ -0,0 +1,62 @@
+/***************************************************************************************************
+ * @file  Terrain.cpp
+ * @brief Implementation of the Terrain class
+ **************************************************************************************************/
+
+#include "mesh/Terrain.hpp"
+
+#include <glad/glad.h>
+
+Terrain::Terrain(const Shader& shader, float chunk_size, unsigned int chunks_on_line)
+    : shader(shader), chunk_size(chunk_size), chunks_on_line(chunks_on_line) {
+    float corner_coord = chunk_size * chunks_on_line / 2.0f;
+    for(unsigned int i = 0 ; i <= chunks_on_line ; ++i) {
+        for(unsigned int j = 0 ; j <= chunks_on_line ; ++j) {
+            positions.emplace_back(-corner_coord + i * chunk_size, -corner_coord + j * chunk_size);
+        }
+    }
+
+    for(unsigned int i = 0 ; i < chunks_on_line ; ++i) {
+        for(unsigned int j = 0 ; j < chunks_on_line ; ++j) {
+            indices.push_back(get_index(i, j + 1));
+            indices.push_back(get_index(i, j));
+            indices.push_back(get_index(i + 1, j));
+            indices.push_back(get_index(i + 1, j + 1));
+        }
+    }
+
+    /* VAO */
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    /* VBO */
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, positions.size() * sizeof(vec2), positions.data(), GL_STATIC_DRAW);
+
+    /* Positions */
+    glVertexAttribPointer(0, 2, GL_FLOAT, false, sizeof(vec2), reinterpret_cast<void*>(0));
+    glEnableVertexAttribArray(0);
+
+    /* Indices & EBO */
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(uint), indices.data(), GL_STATIC_DRAW);
+}
+
+Terrain::~Terrain() {
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+}
+
+void Terrain::draw(const mat4& view_projection) const {
+    shader.use();
+    shader.set_uniform("u_view_projection", view_projection);
+    glBindVertexArray(VAO);
+    glDrawElements(GL_PATCHES, indices.size(), GL_UNSIGNED_INT, nullptr);
+}
+
+unsigned int Terrain::get_index(unsigned int x, unsigned int y) const {
+    return x + y * (chunks_on_line + 1);
+}
