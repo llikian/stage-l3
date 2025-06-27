23134bd972ca9e5b9dd7e0ad10c16185e85f6fbc
llikian <pokillian64@gmail.com>
2025-06-27 12:55:32 +0200

    uvec4 template instantiation of vector4

--- include/maths/vec4.hpp
+++ include/maths/vec4.hpp
@@ -10,7 +10,7 @@
 using vec4 = vector4<float>;
 // using dvec4 = vector4<double>;
 // using ivec4 = vector4<int>;
-// using uvec4 = vector4<unsigned int>;
+using uvec4 = vector4<unsigned int>;
 // using lvec4 = vector4<long>;
 // using llvec4 = vector4<long long>;
 // using ulvec4 = vector4<unsigned long>;
