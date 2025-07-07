edd9d7e363c8784e4699b2ffa50ac83783abcca7
llikian <pokillian64@gmail.com>
2025-07-07 14:06:04 +0200

    Quaternion struct

---
+++ include/maths/quaternion.hpp
@@ -0,0 +1,184 @@
+/***************************************************************************************************
+ * @file  quaternion.hpp
+ * @brief Declaration of the quaternion struct
+ **************************************************************************************************/
+
+#pragma once
+
+#include "mat4.hpp"
+
+/**
+ * @class quaternion
+ * @brief Represents a quaternion with each of its four coefficients (x, y, z, w) such that:\n
+ * q = x*i + y*j + z*k + d.
+ */
+struct quaternion {
+    /**
+     * @brief Constructs a quaternion with all coefficients set to 0.
+     */
+    quaternion();
+
+    /**
+     * @brief Constructs a quaternion with a specific value for each coefficient.
+     * @param x The value for the coefficient of the i imaginary unit.
+     * @param y The value for the coefficient of the j imaginary unit.
+     * @param z The value for the coefficient of the k imaginary unit.
+     * @param w The value for the real part of the quaternion.
+     */
+    quaternion(float x, float y, float z, float w);
+
+    /**
+     * @brief Adds another quaternion to this one.
+     * @param q The quaternion to add.
+     * @return A reference to this quaternion.
+     */
+    quaternion& operator +=(const quaternion& q);
+
+    /**
+     * @brief Subtracts another quaternion from this one.
+     * @param q The quaternion to subtract.
+     * @return A reference to this quaternion.
+     */
+    quaternion& operator -=(const quaternion& q);
+
+    /**
+     * @brief Multiplies another quaternion with this one.
+     * @param q The quaternion to multiply with.
+     * @return A reference to this quaternion.
+     */
+    quaternion& operator *=(const quaternion& q);
+
+    /**
+     * @brief Divides another quaternion from this one.
+     * @param q The quaternion to divide with.
+     * @return A reference to this quaternion.
+     */
+    quaternion& operator /=(const quaternion& q);
+
+    /**
+     * @brief Multiplies this quaternion by a scalar.
+     * @param scalar The scalar to multiply with.
+     * @return A reference to this quaternion.
+     */
+    quaternion& operator *=(float scalar);
+    /**
+     * @brief Divides this quaternion by a scalar.
+     * @param scalar The scalar to divide with.
+     * @return A reference to this quaternion.
+     */
+    quaternion& operator /=(float scalar);
+
+    /**
+     * @brief Calculates the length (norm) of the quaternion.
+     * @return The length of the quaternion.
+     */
+    float get_length() const;
+
+    /**
+     * @brief Calculates the conjugate of the quaternion.
+     * @return The conjugate of the quaternion: q* = (-x, -y, -z, w).
+     */
+    quaternion get_conjugate() const;
+
+    /**
+     * @brief Calculates the inverse of the quaternion.
+     * @return The inverse of the quaternion: q^(-1) = (q*) / norm² = (-x, -y, -z, w) / norm²
+     */
+    quaternion get_inverse() const;
+
+    /**
+     * @brief Calculates the matrix form of the quaternion.
+     * @return The matrix form of the quaternion.
+     */
+    mat4 get_matrix() const;
+
+    /**
+     * @brief Normalizes the quaternion: divides it by its norm so that it has a norm of 1.
+     */
+    void normalize();
+
+    float x; ///< The coefficient of the i imaginary unit.
+    float y; ///< The coefficient of the j imaginary unit.
+    float z; ///< The coefficient of the k imaginary unit.
+    float w; ///< The real part of the quaternion.
+};
+
+/**
+ * @brief Outputs a quaternion to an output stream with the following format: "xi + yj + zk + w".
+ * @param stream The output stream to output to.
+ * @param q The quaternion to output.
+ * @return A reference to the output stream.
+ */
+std::ostream& operator <<(std::ostream& stream, const quaternion& q);
+
+/**
+ * @brief Reads four values from an output stream into a quaternion's coefficients.
+ * @param stream The input stream to read input from.
+ * @param q The quaternion to read into.
+ * @return A reference to the input stream.
+ */
+std::istream& operator >>(std::istream& stream, quaternion& q);
+
+/**
+ * @brief Add two quaternions together.
+ * @param q The first quaternion.
+ * @param r The second quaternion.
+ * @return The sum of the two quaternions.
+ */
+quaternion operator +(const quaternion& q, const quaternion& r);
+
+/**
+ * @brief Subtract a quaternion from another.
+ * @param q The quaternion to subtract from.
+ * @param r The quaternion to subtract with.
+ * @return The subtraction of the first quaternion by the second.
+ */
+quaternion operator -(const quaternion& q, const quaternion& r);
+
+/**
+ * @brief Multiplies two quaternions together. Multiplication is not commutative for quaternions.
+ * @param q The left quaternion.
+ * @param r The right quaternion.
+ * @return The product of the first quaternion with the second.
+ */
+quaternion operator *(const quaternion& q, const quaternion& r);
+
+/**
+ * @brief Divides a quaternion by another.
+ * @param q The quaternion to divide from.
+ * @param r The quaternion to divide with.
+ * @return The division of the first quaternion by the second.
+ */
+quaternion operator /(const quaternion& q, const quaternion& r);
+
+/**
+ * @brief Multiplies a quaternion by a scalar.
+ * @param q The quaternion to multiply by the scalar.
+ * @param scalar The scalar to multiply the quaternion with.
+ * @return The product of the quaternion by the scalar.
+ */
+quaternion operator *(const quaternion& q, float scalar);
+
+/**
+ * @brief Multiplies a quaternion by a scalar.
+ * @param q The quaternion to multiply by the scalar.
+ * @param scalar The scalar to multiply the quaternion with.
+ * @return The product of the quaternion by the scalar.
+ */
+quaternion operator *(float scalar, const quaternion& q);
+
+/**
+ * @brief Divides a quaternion by a scalar.
+ * @param q The quaternion to divide by the scalar.
+ * @param scalar The scalar to divide the quaternion with.
+ * @return The division of the quaternion by the scalar.
+ */
+quaternion operator /(const quaternion& q, float scalar);
+
+/**
+ * @brief Divides a scalar by a quaternion.
+ * @param q The quaternion to divide the scalar with.
+ * @param scalar The scalar to divide by the quaternion.
+ * @return The division of the scalar by the quaternion.
+ */
+quaternion operator /(float scalar, const quaternion& q);
---
+++ src/maths/quaternion.cpp
@@ -0,0 +1,150 @@
+/***************************************************************************************************
+ * @file  quaternion.cpp
+ * @brief Implementation of the quaternion struct
+ **************************************************************************************************/
+
+#include "maths/quaternion.hpp"
+
+#include <cmath>
+
+quaternion::quaternion() : x(0.0f), y(0.0f), z(0.0f), w(0.0f) { }
+
+quaternion::quaternion(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) { }
+
+quaternion& quaternion::operator +=(const quaternion& q) {
+    x += q.x;
+    y += q.y;
+    z += q.z;
+    w += q.w;
+
+    return *this;
+}
+
+quaternion& quaternion::operator -=(const quaternion& q) {
+    x -= q.x;
+    y -= q.y;
+    z -= q.z;
+    w -= q.w;
+
+    return *this;;
+}
+
+quaternion& quaternion::operator *=(const quaternion& q) {
+    x = y * q.z - z * q.y + x * q.w + w * q.x;
+    y = z * q.x - x * q.z + y * q.w + w * q.y;
+    z = x * q.y - y * q.x + z * q.w + w * q.z;
+    w = w * q.w - x * q.x - y * q.y - z * q.z;
+
+    return *this;
+}
+
+quaternion& quaternion::operator /=(const quaternion& q) {
+    quaternion inv = q.get_inverse();
+
+    x = y * inv.z - z * inv.y + x * inv.w + w * inv.x;
+    y = z * inv.x - x * inv.z + y * inv.w + w * inv.y;
+    z = x * inv.y - y * inv.x + z * inv.w + w * inv.z;
+    w = w * inv.w - x * inv.x - y * inv.y - z * inv.z;
+
+    return *this;
+}
+
+quaternion& quaternion::operator *=(float scalar) {
+    x *= scalar;
+    y *= scalar;
+    z *= scalar;
+    w *= scalar;
+
+    return *this;;
+}
+
+quaternion& quaternion::operator /=(float scalar) {
+    x /= scalar;
+    y /= scalar;
+    z /= scalar;
+    w /= scalar;
+
+    return *this;;
+}
+
+float quaternion::get_length() const {
+    return std::sqrt(x * x + y * y + z * z + w * w);
+}
+
+quaternion quaternion::get_conjugate() const {
+    return quaternion(-x, -y, -z, w);
+}
+
+quaternion quaternion::get_inverse() const {
+    float length = get_length();
+    length = 1.0f / (length * length);
+    return quaternion(-length * x, -length * y, -length * z, length * w);
+}
+
+mat4 quaternion::get_matrix() const {
+    float length = get_length();
+    float s = 2.0f / (length * length);
+    return mat4(
+        1.0f - s * (y * y + z * z), s * (x * y - w * z), s * (x * z + w * y),
+        s * (x * y + w * z), 1.0f - s * (x * x + z * z), s * (y * z - w * x),
+        s * (x * z - w * y), s * (y * z + w * x), 1.0f - s * (x * x + y * y)
+    );
+}
+
+void quaternion::normalize() {
+    float length = get_length();
+    x /= length;
+    y /= length;
+    z /= length;
+    w /= length;
+}
+
+std::ostream& operator <<(std::ostream& stream, const quaternion& q) {
+    std::cout << q.x << 'i'
+              << ((q.y >= 0) ? " + " : " - ") << fabs(q.y) << 'j'
+              << ((q.z >= 0) ? " + " : " - ") << fabs(q.y) << 'k'
+              << ((q.w >= 0) ? " + " : " - ") << fabs(q.y);
+
+    return stream;
+}
+
+std::istream& operator >>(std::istream& stream, quaternion& q) {
+    return stream >> q.x >> q.y >> q.z >> q.w;
+}
+
+quaternion operator +(const quaternion& q, const quaternion& r) {
+    return quaternion(q.x + r.x, q.y + r.y, q.z + r.z, q.w + r.w);
+}
+
+quaternion operator -(const quaternion& q, const quaternion& r) {
+    return quaternion(q.x - r.x, q.y - r.y, q.z - r.z, q.w - r.w);
+}
+
+quaternion operator *(const quaternion& q, const quaternion& r) {
+    return quaternion(
+        q.y * r.z - q.z * r.y + q.x * r.w + q.w * r.x,
+        q.z * r.x - q.x * r.z + q.y * r.w + q.w * r.y,
+        q.x * r.y - q.y * r.x + q.z * r.w + q.w * r.z,
+        q.w * r.w - q.x * r.x - q.y * r.y - q.z * r.z
+    );
+}
+
+quaternion operator /(const quaternion& q, const quaternion& r) {
+    return q * r.get_inverse();
+}
+
+quaternion operator *(const quaternion& q, float scalar) {
+    return quaternion(q.x * scalar, q.y * scalar, q.z * scalar, q.w * scalar);
+}
+
+quaternion operator *(float scalar, const quaternion& q) {
+    return quaternion(scalar * q.x, scalar * q.y, scalar * q.z, scalar * q.w);
+}
+
+quaternion operator /(const quaternion& q, float scalar) {
+    return quaternion(q.x / scalar, q.y / scalar, q.z / scalar, q.w / scalar);
+}
+
+quaternion operator /(float scalar, const quaternion& q) {
+    return scalar * q.get_inverse();
+}
