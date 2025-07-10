442e4177e5ace0f0808e2da29ba7f6bf96275190
llikian <pokillian64@gmail.com>
2025-07-10 13:17:16 +0200

    Mettalic Roughness Material struct

---
+++ include/mesh/MRMaterial.hpp
@@ -0,0 +1,21 @@
+/***************************************************************************************************
+ * @file  MRMaterial.hpp
+ * @brief Declaration of the MRMaterial class
+ **************************************************************************************************/
+
+#pragma once
+#include "maths/vec4.hpp"
+#include "Texture.hpp"
+
+/**
+ * @class MRMaterial
+ * @brief Metallic-Roughness material.
+ */
+struct MRMaterial {
+    Texture albedo_map;
+    Texture metallic_roughness_map;
+    vec4 albedo;
+    float metallic;
+    float roughness;
+    float fresnel0;
+};
